bdd65d25b86ff67daca1015c3c3e7049e6063455
Ramesh Rs <rameshrs@centizen.com>
2024-08-06 16:46:38 +0530

    New Message

--- src/main/java/org/example/oops_1/Student.java
+++ src/main/java/org/example/oops_1/Student.java
@@ -1,12 +1,13 @@
 package org.example.oops_1;
 
 public class Student {
-    int age ;
+    protected int age ;
     String name ;
 
     public void changeBatch() {
         System.out.println("change Batch");
     }
+
     public void joinClass(){
         System.out.println(name +" Welcome to our class");
     }
---
+++ src/main/java/org/example/oops_1/Teacher.java
@@ -0,0 +1,8 @@
+package org.example.oops_1;
+
+public class Teacher {
+    Student st = new Student();
+    public void studentAge(){
+        System.out.println(st.age);
+    }
+}
